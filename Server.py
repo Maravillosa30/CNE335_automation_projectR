4b44ee76c7f24a66fe25cd7f62c8014b11d59bf7
Rosa Hulbert <rmhulbert@student.rtc.edu>
2023-11-29 18:29:45 -0800

    update 11/29/23
    push to origin

--- Server.py
+++ Server.py
@@ -1,10 +1,25 @@
+import os
+
+
 class Server:
     """ Server class for representing and manipulating servers. """
 
     def __init__(self, server_ip):
-        # TODO -
         self.server_ip = server_ip
 
     def ping(self):
         # TODO - Use os module to ping the server
-        return
+        response = os.system(f"ping -n 4 {self.server_ip}")
+        # The value of 'response' indicates the success of the ping. 0 means success.
+        if response == 0:
+            return f"The server {self.server_ip} is reachable."
+        else:
+            return f"The server {self.server_ip} is not reachable."
+
+
+# Instantiate the Server class with a server IP
+my_server = Server("35.160.182.172")
+
+# Call the ping method and print the result
+ping_result = my_server.ping()
+print(ping_result)
--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 # This is the template code for the CNE335 Final Project
-# Justin Ellis
+# Rosa Hulbert
 # CNE 335 Fall
+
+import subprocess
+
+
+class Server:
+    def __init__(self, server_ip):
+        self.server_ip = server_ip
+
+    def ping(self):
+        try:
+            result = subprocess.check_output(["ping", "-n", "5", self.server_ip], text=True)
+            return result
+        except subprocess.CalledProcessError as e:
+            return f"Error: {e}"
+
 
 def print_program_info():
     # TODO - Change your name
-    print("Server Automator v0.1 by Your Name")
+    print("Server Automator v0.1 by Rosa Hulbert")
+
 
 # This is the entry point to our program
 if __name__ == '__main__':
     print_program_info()
-    # TODO - Create a Server object
+
+    # # TODO - Create a Server object
+    server_ip = '35.160.182.172'
+    server = Server(server_ip)
+
     # TODO - Call Ping method and print the results
+    ping_result = server.ping()
+    print(f"Ping result for {server_ip}:")
+    print(ping_result)
+
